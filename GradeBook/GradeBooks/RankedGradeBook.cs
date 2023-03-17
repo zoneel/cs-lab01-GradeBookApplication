e291386dfa6880c4079729f2406b2de4c300ea6f
zoneel <40122657+zoneel@users.noreply.github.com>
2023-03-17 18:14:53 +0100

    Overrided GetLetterGrade function in RankedGradeBook.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,30 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             base.Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("You need more than 5 students to calculate letter grading.");
+            }
+
+            int twentyPercentOfStudents = (int)Students.Count * (2 / 10); //calculating how much is 20% of students
+
+            List<double> averageGrades = Students.Select(n => n.AverageGrade).ToList(); //list of avg grades of students
+            averageGrades.OrderBy(n => n); //sorted ascending
+
+
+            if (averageGrades[twentyPercentOfStudents] < averageGrade)
+                return 'A';
+            else if (averageGrades[twentyPercentOfStudents*2] < averageGrade)
+                return 'B';
+            else if (averageGrades[twentyPercentOfStudents*3] < averageGrade)
+                return 'C';
+            else if (averageGrades[twentyPercentOfStudents*4] < averageGrade)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
